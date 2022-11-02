360cbad3c8945bebe0bd6adfd17e4e7977605dc6
Joao <jm.marquescorrea@gmail.com>
2022-11-02 00:24:45 +0000

    exercicios Strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,29 @@
+/* # Exercício 2
+
+Observe a string abaixo.
+
+```jsx
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+```
+
+A partir dela, execute os passos abaixo:
+
+a) Remova o excesso de espaços no final da string;
+
+b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da
+ remoção dos espaços;
+
+c) Substitua os traços `________` por “sticioso”. */
+
+
+
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const novaString = minhaString.trim()
+ 
+console.log("tamnho original", minhaString.length)
+console.log("tamanho som recorte", novaString.length)
+
+const novaString1 = novaString.replace("________", "sticioso")
+console.log(novaString1)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,37 @@
+/* # Exercício 3
+
+A partir da frase a seguir, faça o que se pede
+
+```
+Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+```
+
+a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+
+b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+
+c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+
+**EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS” */
+
+
+
+
+
 //Crie a const para a frase aqui
+
+const aviso = `Jorge tem uma casa verde e com portão azul, com os dizeres:
+ "BOAS VINDAS, mas não deixe o gato sair"`
+
+ console.log(aviso)
+ const cor = aviso.replace("verde", "rosa").replace("azul","laranja")
+ console.log(cor) 
+ console.log(cor.includes("verde"))
+ console.log(cor.includes("laranja"))
+ 
+ const frase = "mas não deixe o gato sair".toUpperCase()
+ //console.log(frase.toLocaleUpperCase())
+
+ const frase2 = `Jorge tem uma casa verde e com portão azul, com os dizeres:"${frase}"`
+
+ console.log(frase2)
